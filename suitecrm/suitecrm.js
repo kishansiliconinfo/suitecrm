d8014f54e66f10843146cc0745bae18286bf91cc
kishansiliconinfo <kishan@siliconinfo.com>
2018-01-03 16:50:53 +0530

    Add files via upload

---
+++ suitecrm/suitecrm.js
@@ -0,0 +1,148 @@
+module.exports = function(RED) {
+    function SuitecrmFind(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        
+        var sugar = require('node-sugarcrm-client');
+        
+        var model = config.model;
+        
+        sugar.init(
+			{
+				apiURL:  "http://localhost/SuiteCRM-7.9.8/service/v4_1/rest.php"
+			   ,login:   "admin"
+			   ,passwd:  "admin123"
+			}
+		);
+		
+		//console.log('============Login==========');
+		sugar.login(function(sessionID){
+			if (sessionID != 'undefined') {
+
+				// If you are here, you got a session ID
+				// and you can add all your query here
+				
+				// console.log('Your session ID is', sessionID);
+				
+				modelparams = {
+					 session:  sessionID
+					,filter : "default"
+				};
+				
+				// model list
+				RED.httpAdmin.get("/CRMModels", function(req,res) {
+					sugar.call("get_available_modules", modelparams, function(data,err){
+						if (err) {
+							res.json(err);
+						}
+						else {
+							res.json(data);
+							//console.log(data)
+						}
+					});
+				});
+				
+				modelfieldsparams = {
+						 session:  sessionID
+						,module_name : model
+						,fields: []
+				};
+				
+				// console.log("=====modelfieldsparams=====");
+				// console.log(modelfieldsparams);
+			
+				node.on('input', function(msg) {
+					sugar.call("get_module_fields", modelfieldsparams, function(res,err){
+						if (err) {
+							//console.log(err)
+							node.status({color: 'red', text: err});
+							node.send([null, msg]);
+						}
+						else {
+							//console.log(res)
+							msg.payload = res;
+							node.send([msg, null]);
+							return res;
+						}
+					})
+				});
+				
+				
+				//Model fields
+				RED.httpAdmin.get("/CRMModelsFields/:model", function(req,res) {
+					
+					modelfieldsparams = {
+						 session:  sessionID
+						,module_name: req.params.model
+						,fields: []
+					};
+				
+					// console.log("=====modelfieldsparams=====");
+					// console.log(modelfieldsparams);
+				
+					sugar.call("get_module_fields", modelfieldsparams, function(data,err){
+						if (err) {
+							res.json(err);
+						}
+						else {
+							res.json(data);
+						}
+					});
+				});
+				
+				//Relation list
+				RED.httpAdmin.get("/CRMModelsRelations/:model", function(req,res) {
+					
+					relationparams = {
+						session:  sessionID
+						,module_name: req.params.model
+						,related_fields: []
+					};	
+					
+					sugar.call("get_relationships", relationparams, function(data,err){
+						if (err) {
+							res.json(err);
+						}
+						else {
+							res.json(data);
+							//console.log(data)
+						}
+					});
+				});
+				
+				
+				//Common testing other API list
+				RED.httpAdmin.get("/common/:model", function(req,res) {
+					
+					common = {
+						session:  sessionID
+						,module_name: req.params.model
+						,query: ''
+						,order_by : ''
+						,offset : 0
+						,select_fields: ['id']
+						,link_name_to_fields_array: [['name']]
+						,max_results: 100
+						,deleted: 0
+						,favorites: false
+					};	
+					
+					sugar.call("get_entry_list", common, function(data,err){
+						if (err) {
+							res.json(err);
+							//console.log(err)
+						}
+						else {
+							res.json(data);
+							//console.log(data)
+						}
+					});
+				});
+				
+			} else {
+				//console.log("can't login, check your credentials");
+			}
+		});
+    }
+    RED.nodes.registerType("suitecrm",SuitecrmFind);
+}
